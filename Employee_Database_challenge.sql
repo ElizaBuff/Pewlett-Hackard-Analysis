ee29e79f977edb32361775f9024d2b8839c7084d
ElizaBuff <102329557+ElizaBuff@users.noreply.github.com>
2022-06-05 14:19:48 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -60,7 +60,7 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
-
+--DELIVERABLE 1: The Number of Retiring Employees by Title
 --retirement_titles table 
 SELECT e.emp_no, --#1
     e.first_name,
@@ -106,3 +106,25 @@ GROUP BY title --#19
 ORDER BY COUNT(first_name) DESC; 
 -- Check the table
 SELECT * FROM retiring_titles
+
+--DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+--mentorship_eligibilty table 
+SELECT DISTINCT ON (emp_no) --#4
+e. emp_no, --#1
+e. first_name,
+e. last_name,
+e. birth_date,
+de. from_date, --#2
+de. to_date,
+titles.title --#3
+INTO mentorship_eligibilty --#5
+FROM employees as e
+INNER JOIN dept_emp as de --#6
+ON e.emp_no = de.emp_no --#7
+INNER JOIN titles
+ON e.emp_no = titles.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')--#8
+ORDER BY emp_no; --#9
+-- Check the table
+SELECT * FROM mentorship_eligibilty; 
